a8bc16371471e6f9aeb4fd6bed987bcfd525d4d7
kubula316 <161055506+kubula316@users.noreply.github.com>
2025-10-22 19:50:45 +0200

    Add files via upload

---
+++ Backend/user/src/main/java/com/BINM/user/service/EmailService.java
@@ -0,0 +1,44 @@
+package com.BINM.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendWelcomeEmail(String toEmail, String name) {
+        System.out.println(fromEmail);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Welcome to Our Platform!");
+        message.setText("Hi " + name + ",\n\nThank you for registering with us! We're excited to have you on board.\n\nBest regards,\nBINM Team");
+        mailSender.send(message);
+    }
+
+    public void sendResetOtpEmail(String toEmail, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Password Reset OTP");
+        message.setText("Your OTP for password reset is: " + otp + "\n\nUse this OTP to proceed with reseting your password.");
+        mailSender.send(message);
+    }
+
+    public void sendOtpEmail(String toEmail, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Verify Your Account OTP");
+        message.setText("Your Account Verification OTP code is: " + otp + "\n\nUse this code to proceed with your action.");
+        mailSender.send(message);
+    }
+}
